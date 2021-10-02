0e0cd0e823a3f61e78487d83bf055de1afba91db
FarooqHassan <farooq.hassan@esend.fi>
2021-10-02 16:17:18 +0300

    added way to handle json

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 const todoRouter = require("./todo/todo.routes");
-app.use(express.raw({ type: "application/json" }));
 
 app.use(cors())
+app.use(express.json())
+
 app.use("/todo", todoRouter);
 
 
