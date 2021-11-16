de6eb08319bf3be8924cd44d7aa9d6f9595fd832
FarooqHassan <farooq.hassan@esend.fi>
2021-11-16 10:49:57 +0200

    renaming testing files

---
+++ client/src/pages/snapshot.test.tsx
@@ -0,0 +1,15 @@
+import {
+    render,
+} from "@testing-library/react";
+import Enzyme from "enzyme";
+import Todos from "./Todos";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<Todos/>", () => {
+    it("renders with Props", async () => {
+        const wrapper = render(<Todos />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
